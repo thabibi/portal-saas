5b1779c19e455765112a12340948b7b643de17ff
Turmudi Habibi <turmudihabibi@gmail.com>
2026-03-04 09:44:54 +0000

    Phase 2 - Business model and relationships created

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function business()
+    {
+    return $this->belongsTo(Business::class);
+    }
 }
