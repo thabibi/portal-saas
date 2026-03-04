effb1f42c449dfd4d1a025c583ecbd0cabc6e1bc
Turmudi Habibi <turmudihabibi@gmail.com>
2026-03-04 10:22:35 +0000

    Phase 3 - Global scope added to Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['business_id', 'name', 'price'];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('business', function (Builder $builder) {
+            if (session()->has('business_id')) {
+                $builder->where('business_id', session('business_id'));
+            }
+        });
+    }
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
 }
