b0a826fcf6231f752add88ff90acbead7fb1225a
Turmudi Habibi <turmudihabibi@gmail.com>
2026-03-06 10:10:41 +0000

    Phase 5 - Business selection routing and middleware test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Helpers\BusinessContext;
+use App\Models\Business;
+
+
+//Menampilkan daftar bisnis yang dipiih user
+Route::get('/select-business', function (){
+    $businesses = Business::all();
+    return "Halaman Pilih Bisnis";
+
+})->name('business.select');
+
+//Untuk simpan session bisnis id yang dipilih kemudian ke dashboard
+Route::post('/select-business/{id}', function ($id){
+    BusinessContext::set($id);
+    return redirect('/dashboard');
+
+})->name('busines.set');
+
+Route::middleware(['business.selected'])->group(function () {
+    Route::get('/dashboard', function () {
+        $businessId = \App\Helpers\BusinessContext::get();
+
+        return "Dashboard Business";
+    });
+});
 
 Route::get('/', function () {
     return view('welcome');
